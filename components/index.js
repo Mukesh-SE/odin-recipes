eb09c180670f9d6c215febbe68a531fe37484e3f
Mukesh-SE <mukeshodhano2018@gmail.com>
2023-09-12 06:56:41 +0500

    some changes

---
+++ components/index.js
@@ -0,0 +1,29 @@
+const move = document.getElementById("moveable");
+const nav_itmes = document.querySelectorAll("li");
+
+let move_left = 0;
+nav_itmes.forEach((item, index) => {
+	item.addEventListener("click", () => {
+		switch (index) {
+			case 0:
+				move_left = 2;
+				move.style.left = `${move_left}%`;
+				break;
+			case 1:
+				move_left = 23;
+				move.style.left = `${move_left}%`;
+				break;
+			case 2:
+				move_left = 48.6;
+				move.style.left = `${move_left}%`;
+				break;
+			case 3:
+				move_left = 73.6;
+				move.style.left = `${move_left}%`;
+				break;
+			default:
+				alert("some thing is wrong\n check dev tool");
+				console.log(nav_itmes);
+		}
+	});
+});
