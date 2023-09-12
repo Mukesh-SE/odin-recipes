06adee90881f1bd9dfbab72ba987f01f8d9bcccb
Mukesh-SE <mukeshodhano2018@gmail.com>
2023-09-12 09:45:11 +0500

    ratting and like function

--- script.js
+++ script.js
@@ -6,21 +6,55 @@ list.forEach((item, index) => {
 	item.addEventListener("mouseover", () => {
 		switch (index) {
 			case 0:
-				move_left = 2 + (index * 33.4);
+				move_left = 2 + index * 33.4;
 				focus_div.style.left = `${move_left}%`;
 				break;
 			case 1:
-				move_left = 2 + (index * 33.4);
+				move_left = 2 + index * 33.4;
 				focus_div.style.left = `${move_left}%`;
 				break;
 			case 2:
-				move_left = 2 + (index * 33.4);
+				move_left = 2 + index * 33.4;
 				focus_div.style.left = `${move_left}%`;
 				break;
 			default:
 				alert("somthing is wrong \n check the console");
-				console.log(index, item);
+			// console.log(index, item);
 		}
-		console.log(index, focus_div.style.left);
+		// console.log(index, focus_div.style.left);
 	});
 });
+// -----------------------
+
+// ratting and like
+const rate_star = document.querySelectorAll(".fa-star");
+const heart = document.querySelectorAll(".fa-heart");
+
+heart.forEach((item) => {
+	item.addEventListener("click", () => {
+		item.classList.toggle("fas");
+	});
+});
+
+rate_star.forEach((item, index) => {
+	item.addEventListener("click", () => {
+		toogle_star(index);
+	});
+});
+function toogle_star(clickeIndex) {
+	let start_index = 0;
+	if (clickeIndex > 4) {
+		start_index = 5;
+	}
+	if (clickeIndex > 9) {
+		start_index = 10;
+	}
+	// console.log(clickeIndex);
+	for (let i = start_index; i <= clickeIndex; i++) {
+		if (rate_star[i].classList.contains("fas")) {
+			rate_star[i].classList.remove("fas");
+		}
+		rate_star[i].classList.add("fas");
+	}
+}
+// -----------------------
