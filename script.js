bfa64489303cfff5a797f257e8f1de3cd8e818b4
Mukesh-SE <mukeshodhano2018@gmail.com>
2023-09-12 08:23:50 +0500

    css

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const list = document.querySelectorAll("li");
 const focus_div = document.getElementById("hv");
 
-let move_left = 60;
+let move_left = 0;
 list.forEach((item, index) => {
 	item.addEventListener("mouseover", () => {
 		switch (index) {
 			case 0:
-				move_left = 2.45 + (index * 33.3);
+				move_left = 2 + (index * 33.4);
 				focus_div.style.left = `${move_left}%`;
 				break;
 			case 1:
-				move_left = 2.47 + (index * 33.3);
+				move_left = 2 + (index * 33.4);
 				focus_div.style.left = `${move_left}%`;
 				break;
 			case 2:
-				move_left = 2.47 + (index * 33.3);
+				move_left = 2 + (index * 33.4);
 				focus_div.style.left = `${move_left}%`;
 				break;
 			default:
